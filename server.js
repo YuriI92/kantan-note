049b88d35f1e018842286caa4e98c8c2e557714d
Yurippe <yurichikawa1992@gmail.com>
2022-05-06 18:19:40 -0400

    add api routes for get notes and post new note

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const path = require('path');
+const { nanoid } = require('nanoid');
+const { notes } = require('./db/db.json');
+const createNewNote = require('./lib/notes');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = nanoid();
+    res.json(createNewNote(req.body, notes));
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
