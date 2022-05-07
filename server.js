191e5492222ebc0ab1adf2eb0d2837244b4d29d8
Yurippe <yurichikawa1992@gmail.com>
2022-05-07 15:28:42 -0400

    separated routes into routes folder

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const path = require('path');
-const { nanoid } = require('nanoid');
-const { createNewNote, deleteNote } = require('./lib/notes');
-const { readFile } = require('fs/promises');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,34 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/api/notes', async (req, res) => {
-    let data = await readFile('./db/db.json','utf-8');
-    console.log(data);
-    res.json(JSON.parse(data));
-});
-
-app.post('/api/notes', async (req, res) => {
-    let data = await readFile('./db/db.json','utf-8');
-    req.body.id = nanoid();
-    res.json(createNewNote(req.body, JSON.parse(data)));
-});
-
-app.delete('/api/notes/:id', async (req, res) => {
-    let data = await readFile('./db/db.json','utf-8');
-    res.json(deleteNote(req.params.id, JSON.parse(data)));
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
