525cf7015d20f54411009b4df9ff71e50b5d7eae
Yurippe <yurichikawa1992@gmail.com>
2022-05-07 14:40:39 -0400

    change json file format

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -119,14 +119,12 @@ const handleRenderSaveBtn = () => {
 // Render the list of note titles
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
+
   if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
 
-  console.log(jsonNotes);
-
   let noteListItems = [];
-  console.log(noteListItems);
 
   // Returns HTML element with or without a delete button
   const createLi = (text, delBtn = true) => {
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+// const fs = require('fs');
 const { nanoid } = require('nanoid');
-const { notes } = require('./db/db.json');
 const { createNewNote, deleteNote } = require('./lib/notes');
+const notes = require('./db/db.json');
+const { readFile } = require('fs/promises');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,17 +13,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
+app.get('/api/notes', async (req, res) => {
+    let data = await readFile('./db/db.json','utf-8');
+    console.log(data);
+    res.json(JSON.parse(data));
 });
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', async (req, res) => {
+    let data = await readFile('./db/db.json','utf-8');
     req.body.id = nanoid();
-    res.json(createNewNote(req.body, notes));
+    res.json(createNewNote(req.body, JSON.parse(data)));
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-    res.json(deleteNote(req.params.id, notes));
+app.delete('/api/notes/:id', async (req, res) => {
+    let data = await readFile('./db/db.json','utf-8');
+    res.json(deleteNote(req.params.id, JSON.parse(data)));
 });
 
 app.get('/', (req, res) => {
