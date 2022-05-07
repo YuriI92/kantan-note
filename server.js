250de4572b5b478ec18dccdfb1627e31e6b60d75
Yurippe <yurichikawa1992@gmail.com>
2022-05-07 19:21:20 -0400

    add comments to server.js and notes.js files

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// api routes and html routes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
